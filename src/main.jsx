4b4faf16573d606151fd994a1b50f79431bd3b5b
fachrihms123 <mitsumiharu79@gmail.com>
2025-11-08 10:31:28 +0700

    redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Route } from "lucide-react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
