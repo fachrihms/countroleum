4bccc4051ccbcd93a6b87d421ddd85771a065601
fachrihms123 <mitsumiharu79@gmail.com>
2025-11-08 19:52:08 +0700

    edit typo capital in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Countroleum from "./Countroleum";
+import Countroleum from "./countroleum";
 
 function App() {
   // NOTE: Anda mungkin perlu menambahkan fungsi "window.storage" global
